3c84790163b495b90015fb9a425e8c7f20ed2bcb
Ardeyu <andrei.dana87@gmail.com>
2018-04-03 23:15:53 +0300

    Add scrollable text

--- PrizeWinner/MainPage.xaml.cs
+++ PrizeWinner/MainPage.xaml.cs
@@ -22,9 +22,34 @@ namespace PrizeWinner
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private DispatcherTimer timer;
+
         public MainPage()
         {
             this.InitializeComponent();
+            timer = new DispatcherTimer();
+        }
+
+        private void scrollViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer.Tick += (ss, ee) =>
+            {
+                if (timer.Interval.Ticks == 300)
+                {
+                    //each time set the offset to scrollviewer.HorizontalOffset + 5
+                    scrollviewer.ScrollToHorizontalOffset(scrollviewer.HorizontalOffset + 5);
+                    //if the scrollviewer scrolls to the end, scroll it back to the start.
+                    if (scrollviewer.HorizontalOffset == scrollviewer.ScrollableWidth)
+                        scrollviewer.ScrollToHorizontalOffset(0);
+                }
+            };
+            timer.Interval = new TimeSpan(300);
+            timer.Start();
+        }
+
+        private void scrollviewer_Unloaded(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
         }
     }
 }
